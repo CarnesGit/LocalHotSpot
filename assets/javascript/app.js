f2e20e2c3a8d597e0a55caefded728552f75c587
Louis Malone <louismalone@Louiss-MacBook-Air.local>
2019-07-06 10:45:13 -0400

    api calls

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,28 +1,42 @@
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
-    "q=CapeMay,NJ&units=imperial&appid=" + APIKey;
+$(document).ready(function() {
+    $('#createAcct').hide();
 
-// Here we run our AJAX call to the OpenWeatherMap API
-$.ajax({
-        url: queryURL,
-        method: "GET"
-    })
-    // We store all of the retrieved data inside of an object called "response"
-    .then(function(response) {
-
-        // Log the queryURL
-        console.log(queryURL);
-
-        // Log the resulting object
-        console.log(response);
-
-        // Transfer content to HTML
-        $(".city").html("<h1>" + response.name + " Weather Details</h1>");
-        $(".wind").text("Wind Speed: " + response.wind.speed);
-        $(".humidity").text("Humidity: " + response.main.humidity);
-        $(".temp").text("Temperature (F) " + response.main.temp);
-
-        // Log the data in the console as well
-        console.log("Wind Speed: " + response.wind.speed);
-        console.log("Humidity: " + response.main.humidity);
-        console.log("Temperature (F): " + response.main.temp);
+    $('#newAcctBtn').click(function() {
+        $('#signIn').hide();
+        $('#headH1').hide();
+        $('#createAcct').show();
+        $('body').css("background", "url('assets/images/createAcct.jpg') no-repeat center center fixed");
+        $('body').css("-webkit-background-size", "cover");
+        $('body').css("-moz-background-size", "cover");
+        $('body').css("-o-background-size", "cover");
+        $('body').css("background-size", "cover");
     });
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
+        "q=CapeMay,NJ&units=imperial&appid=" + APIKey;
+
+    // Here we run our AJAX call to the OpenWeatherMap API
+    $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+        // We store all of the retrieved data inside of an object called "response"
+        .then(function(response) {
+
+            // Log the queryURL
+            console.log(queryURL);
+
+            // Log the resulting object
+            console.log(response);
+
+            // Transfer content to HTML
+            $(".city").html("<h1>" + response.name + " Weather Details</h1>");
+            $(".wind").text("Wind Speed: " + response.wind.speed);
+            $(".humidity").text("Humidity: " + response.main.humidity);
+            $(".temp").text("Temperature (F) " + response.main.temp);
+
+            // Log the data in the console as well
+            console.log("Wind Speed: " + response.wind.speed);
+            console.log("Humidity: " + response.main.humidity);
+            console.log("Temperature (F): " + response.main.temp);
+        });
+});
