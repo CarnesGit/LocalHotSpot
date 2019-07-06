9379bbb944e56bdaf5520bad0d4af982f8fba025
Rick <rick.carnes@gmail.com>
2019-07-06 13:44:00 -0400

    fixin stuff

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,37 +11,38 @@ $(document).ready(function() {
         $('body').css("-o-background-size", "cover");
         $('body').css("background-size", "cover");
     });
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
-        "q=CapeMay,NJ&units=imperial&appid=" + APIKey;
 
-    var APIKey =
+    // RICK, PUT THE API KEY BACK
+    var APIKey = "e3e26770ea91f1526f1d91e4b4212507";
 
-        // Here we run our AJAX call to the OpenWeatherMap API
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function(response) {
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?zip=08260,us&units=imperial&appid=" + APIKey;
 
-            // We store all of the retrieved data inside of an object called "response"
+    // Here we run our AJAX call to the OpenWeatherMap API
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+
+        // We store all of the retrieved data inside of an object called "response"
 
 
-            // Log the queryURL
-            console.log(queryURL);
+        // Log the queryURL
+        console.log(queryURL);
 
-            // Log the resulting object
-            console.log(response);
+        // Log the resulting object
+        console.log(response);
 
-            // Transfer content to HTML
-            $(".city").html("<h1>" + response.name + " Weather Details</h1>");
-            $(".wind").text("Wind Speed: " + response.wind.speed);
-            $(".humidity").text("Humidity: " + response.main.humidity);
-            $(".temp").text("Temperature (F) " + response.main.temp);
+        // Transfer content to HTML
+        $(".city").html("<h1>" + response.name + " Weather Details</h1>");
+        $(".wind").text("Wind Speed: " + response.wind.speed);
+        $(".humidity").text("Humidity: " + response.main.humidity);
+        $(".temp").text("Temperature (F) " + response.main.temp);
 
-            // Log the data in the console as well
-            console.log("Wind Speed: " + response.wind.speed);
-            console.log("Humidity: " + response.main.humidity);
-            console.log("Temperature (F): " + response.main.temp);
-        });
+        // Log the data in the console as well
+        console.log("Wind Speed: " + response.wind.speed);
+        console.log("Humidity: " + response.main.humidity);
+        console.log("Temperature (F): " + response.main.temp);
+    });
 
 
     $('#createCancelBtn').click(function() {
